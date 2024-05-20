23b394fa6e76e24070d10a50296afb76aefec902
k1sihyeon <tlgus530@naver.com>
2024-05-21 07:12:16 +0900

    [Update] LocalDateTime 직렬화/역직렬화 코드

--- app/src/main/java/kr/ac/kumoh/ce/moducare/data/MLogApi.kt
+++ app/src/main/java/kr/ac/kumoh/ce/moducare/data/MLogApi.kt
@@ -1,5 +1,7 @@
 package kr.ac.kumoh.ce.moducare.data
 
+import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.PATCH
 import retrofit2.http.Path
@@ -12,7 +14,7 @@ interface mLogApi {
     suspend fun getLog(@Path("id") id: Int): mLog
 
     @PATCH("api/logs/{id}/checked")
-    suspend fun checkLog(@Path("id") id: Int)
+    suspend fun checkLog(@Path("id") id: Long, @Body checked: Boolean): mLog
 
     @GET("api/logs/{id}/comments")
     suspend fun getComments(@Path("id") id: Int): List<Comment>
--- app/src/main/java/kr/ac/kumoh/ce/moducare/viewModel/MLogViewModel.kt
+++ app/src/main/java/kr/ac/kumoh/ce/moducare/viewModel/MLogViewModel.kt
@@ -5,16 +5,46 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonPrimitive
+import com.google.gson.JsonSerializationContext
+import com.google.gson.JsonSerializer
 import kotlinx.coroutines.launch
 import kr.ac.kumoh.ce.moducare.data.mLog
 import kr.ac.kumoh.ce.moducare.data.mLogApi
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.reflect.Type
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class LocalDateTimeSerializer : JsonSerializer<LocalDateTime> {
+    override fun serialize(src: LocalDateTime?, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement {
+        val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+        return JsonPrimitive(src?.format(formatter))
+    }
+}
+
+class LocalDateTimeDeserializer : JsonDeserializer<LocalDateTime> {
+    override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): LocalDateTime {
+        val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+        return LocalDateTime.parse(json?.asString, formatter)
+    }
+}
 
 class mLogViewModel() : ViewModel() {
     private val SERVER_URL = "http://118.219.42.214:8080/"
     private val mlogApi: mLogApi
     private val _logList = MutableLiveData<List<mLog>>()
+
+    private val gson = GsonBuilder()
+        .registerTypeAdapter(LocalDateTime::class.java, LocalDateTimeSerializer())
+        .registerTypeAdapter(LocalDateTime::class.java, LocalDateTimeDeserializer())
+        .create()
+
 
     val logList: LiveData<List<mLog>>
         get() = _logList
@@ -22,7 +52,7 @@ class mLogViewModel() : ViewModel() {
     init {
         val retrofit = Retrofit.Builder()
             .baseUrl(SERVER_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
 
         mlogApi = retrofit.create(mLogApi::class.java)
